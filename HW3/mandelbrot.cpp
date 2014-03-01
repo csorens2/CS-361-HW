2d5cda0e2f3b8de70963b5475b1ac80af142ba92
Chris <csorens2@uic.edu>
2014-03-01 15:27:19 -0600

    New Files and Names

---
+++ HW3/mandelbrot.cpp
@@ -0,0 +1,29 @@
+#include "mandelbrot.h"
+
+#include <stdio.h>
+#include <sys/wait.h>
+#include <thread>
+#include <signal.h>
+#include <iostream>
+
+int childpid = -1;
+
+int main(int argc, char** argv)
+{
+	printf("Chris Sorenson \ncsorens2 \n");
+	childpid = fork();
+	if(childpid == 0) //Children processes
+	{
+		childpid = fork();
+		if(childpid == 0) //First child process
+		{
+		}
+		else //Second Child Process
+		{
+		}
+	}
+	else //Parent Process
+	{
+			
+	}
+}
---
+++ HW3/mandelbrot.h
@@ -0,0 +1,8 @@
+/*
+* Christopher Sorenson
+* csorens2
+*/
+#ifndef MANDELBROT_H_
+#define MANDELBROT_H_
+
+#endif
