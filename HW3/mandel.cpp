c69cd364dbefba4871d9e27b2dbcf8cd65475939
Chris <csorens2@uic.edu>
2014-03-01 14:38:51 -0600

    Added cpp and h

---
+++ HW3/mandel.cpp
@@ -0,0 +1,20 @@
+#include "mandel.h"
+
+#include <stdio.h>
+#include <sys/wait.h>
+#include <thread>
+#include <signal.h>
+#include <iostream>
+
+int main()
+{
+}
+
+void firstChild()
+{
+}
+
+void secondChild()
+{
+}
+
---
+++ HW3/mandel.h
@@ -0,0 +1,11 @@
+/*
+* Christopher Sorenson
+* csorens2
+*/
+#ifndef MANDEL_H_
+#define MANDEL_H_
+
+static void firstChild();
+static void secondChild();
+
+#endif
